b48f35dd0be6d442eeb3fd8c6ddf27a3942dae44
Ian Park <ianpark@Ians-MacBook-Pro.local>
2025-05-02 15:53:51 +0800

    Update v1.1.0

--- GISGuessr/compress.py
+++ GISGuessr/compress.py
@@ -1,9 +1,8 @@
 import os
 from PIL import Image
 
-for i in os.listdir('gen2/ArtsBlock'):
-    if i in os.listdir('gen2'):
+for i in os.listdir('output3'):
+    if i in os.listdir('gen3'):
         continue
-    im = Image.open('gen2/ArtsBlock/' + i)
-    im.resize((1008, 756))
-    im.save('gen2/' + i)
+    im = Image.open('output3/' + i).resize((1008, 756))
+    im.save('gen3/' + i[:-4] + ".jpg", quality=75)
